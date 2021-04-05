edf961da014ebed5e58538ad846a39aadc39ce2e
Anton <toxa1507@gmail.com>
2021-04-05 21:24:25 +0300

    sdfdsf

--- HW1/1/index.js
+++
@@ -1,8 +0,0 @@
-for (let i=1;i<=10;i++){
-    if ((i%3)==0){console.log('FizBuz')}
-    else if ((i%2)==0){console.log('Fiz')}
-    else if ((i%2)==!0){console.log('Buz')}
-}
-////               
-for(let i=10;i<=1;i*(i-1))
-{console.log(let)}
